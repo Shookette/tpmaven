0895502435e9a0f893c98d609952cc713e4e15e6
blackstylle <viallattej@gmail.com>
2015-01-06 15:10:45 +0100

    test commit all file

---
+++ src/main/java/fr/istic/sir/tpmaven/App.java
@@ -0,0 +1,15 @@
+package fr.istic.sir.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    /**
+     * Main function for App class.
+     * @param args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
